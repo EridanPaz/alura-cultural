51fa2e5c7c34d80fc9617706e9d59f8bbad012a5
Eridan Paz <eribpaz@gmail.com>
2024-07-03 08:19:25 -0300

    Início do projeto.

---
+++ src/main/java/com/alura/alura_cultural/AluraCulturalApplication.java
@@ -0,0 +1,13 @@
+package com.alura.alura_cultural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraCulturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraCulturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/alura_cultural/AluraCulturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.alura_cultural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraCulturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
